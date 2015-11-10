385988eece2379401b1084d025568379899759d9
B0050 <B0050@yungshingroup.com>
2015-11-10 13:02:18 +0800

    加入searchType 欄位屬性 1:一般欄位 2:日期欄位 依據不同類型組出相對應的SQL字串

--- SomeUtils/Bean/QueryItem.java
+++ SomeUtils/Bean/QueryItem.java
@@ -1,32 +1,48 @@
 package SomeUtils.Bean;
 
-
 import java.io.Serializable;
 
-public class QueryItem implements Serializable{
+public class QueryItem implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -67545244671344280L;
-	private String fieldName ="";
+	private String fieldName = "";
 	private String chineseName = "";
-	public QueryItem(String fieldName, String chineseName) {
+	private int searchType;
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public QueryItem(String fieldName, String chineseName, int searchType) {
 		super();
 		this.fieldName = fieldName;
 		this.chineseName = chineseName;
+		this.searchType = searchType;
 	}
-	
+
 	public String getFieldName() {
 		return fieldName;
 	}
+
 	public void setFieldName(String fieldName) {
 		this.fieldName = fieldName;
 	}
+
 	public String getChineseName() {
 		return chineseName;
 	}
+
 	public void setChineseName(String chineseName) {
 		this.chineseName = chineseName;
 	}
-	
+
+	public int getSearchType() {
+		return searchType;
+	}
+
+	public void setSearchType(int searchType) {
+		this.searchType = searchType;
+	}
 }
