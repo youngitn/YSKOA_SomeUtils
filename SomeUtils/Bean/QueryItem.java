875c1c47d46bc055eaa093435b4b31884c2946c2
B0050 <B0050@yungshingroup.com>
2015-11-11 11:26:38 +0800

    加入注解

--- SomeUtils/Bean/QueryItem.java
+++ SomeUtils/Bean/QueryItem.java
@@ -1,11 +1,13 @@
 package SomeUtils.Bean;
 
 import java.io.Serializable;
-
+/**
+ * 容器,裝所要查詢的欄位名稱與中文名稱(資料庫欄位名稱,dmaker欄位標題) 
+ * @author b0050
+ * 
+ */
 public class QueryItem implements Serializable {
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = -67545244671344280L;
 	private String fieldName = "";
 	private String chineseName = "";
@@ -15,6 +17,13 @@ public class QueryItem implements Serializable {
 		return serialVersionUID;
 	}
 
+	/**
+	 * 查詢頁面的UI欄位中,想要當作查詢條件的欄位,用此類別裝載後丟進去ArrayList
+	 * 配合_hproc.setQueryTable()使用.
+	 * @param fieldName 欄位名稱
+	 * @param chineseName 中文名稱
+	 * @param searchType int 0:該欄位不做為查詢條件 1:一般查詢跳健欄位 2:時間區間查詢欄位
+	 */
 	public QueryItem(String fieldName, String chineseName, int searchType) {
 		super();
 		this.fieldName = fieldName;
