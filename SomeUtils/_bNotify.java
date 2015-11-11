47bfbb18a612e71da582331b6536befd4e171aa6
B0050 <B0050@yungshingroup.com>
2015-11-11 15:07:46 +0800

    增加"通知"用類別:bNotify

---
+++ SomeUtils/_bNotify.java
@@ -0,0 +1,17 @@
+package SomeUtils;
+
+import jcx.jform.bNotify;
+/**
+ * 
+ * @author b0050
+ * 用來簡化通知處理的類別.
+ */
+public class _bNotify extends bNotify {
+
+	@Override
+	public void actionPerformed(String arg0) throws Throwable {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
